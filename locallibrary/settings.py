423ff4720342cbf6d88f4e13cb4346160b65c7f6
Vladfsociety <kobylnikvlad@gmail.com>
2019-06-14 13:11:33 +0300

    fdsf

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
+
 # Heroku: Update database configuration from $DATABASE_URL.
 
 db_from_env = dj_database_url.config(conn_max_age=500)
